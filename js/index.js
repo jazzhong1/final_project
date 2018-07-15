7380769f3d5038141bebc790b6244b561e1f34ad
jazzhong1 <36650330+jazzhong1@users.noreply.github.com>
2018-07-15 16:56:11 +0900

    reee

---
+++ js/index.js
@@ -0,0 +1,51 @@
+<!--작은 케러셀-->
+  // Instantiate the Bootstrap carousel
+  $('.multi-item-carousel').carousel({
+    interval: false
+  });
+
+  // for every slide in carousel, copy the next slide's item in the slide.
+  // Do the same for the next, next item.
+  $('.multi-item-carousel .item').each(function() {
+    var next = $(this).next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    if (next.next().length > 0) {
+      next.next().children(':first-child').clone().appendTo($(this));
+    } else {
+      $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+    }
+  });
+
+
+  $(document).ready(function () {
+var trigger = $('.hamburger'),
+    overlay = $('.overlay'),
+   isClosed = false;
+
+  trigger.click(function () {
+    hamburger_cross();
+  });
+
+  function hamburger_cross() {
+
+    if (isClosed == true) {
+      overlay.hide();
+      trigger.removeClass('is-open');
+      trigger.addClass('is-closed');
+      isClosed = false;
+    } else {
+      overlay.show();
+      trigger.removeClass('is-closed');
+      trigger.addClass('is-open');
+      isClosed = true;
+    }
+}
+
+$('[data-toggle="offcanvas"]').click(function () {
+      $('#wrapper').toggleClass('toggled');
+});
+});
